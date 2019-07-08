c1a42165ef13bcf7bfd96a8964e7725347eba252
Simas <simonas.deimantavicius@mediapark.com>
2019-07-08 09:15:24 +0300

    add custom plugin 1/2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Line } from 'react-chartjs-2';
+import { Chart, Bar } from 'react-chartjs-2';
 
 export default class App extends Component {
   constructor(props) {
@@ -7,17 +7,12 @@ export default class App extends Component {
 
     this.state = {
       data: {
-        labels: ['1', '2', '3', '4', '5'],
+        labels: ['January', 'February', 'March', 'April', 'June', 'July'],
         datasets: [
           {
             label: 'Videos Made',
             backgroundColor: 'rgba(255, 0, 255, 0.75)',
-            data: [4, 5, 1, 10, 32, 2, 12],
-          },
-          {
-            label: 'Subscribtions',
-            backgroundColor: 'rgba(0, 255,0,0.75)',
-            data: [14, 15, 21, 0, 12, 34, 42],
+            data: [65, 59, 80, 81, 56, 55, 40],
           },
         ],
       },
@@ -50,11 +45,104 @@ export default class App extends Component {
     return (
       <div style={{ position: 'relative', width: 600, height: 550 }}>
         <h3>React ChartJs</h3>
-        <Line
+        <Bar
           options={{
             responsive: true,
           }}
           data={this.getChartData}
+          plugins={{
+            id: 'curvature',
+            beforeInit: (chart, _easing) => {
+              if (!chart.options.curvature) return;
+
+              Chart.elements.Rectangle.prototype.draw = function() {
+                const ctx = this._chart.ctx;
+                const vm = this._view;
+
+                let left, right, top, bottom, signX, signY, borderSkipped, radius;
+                let borderWidth = vm.borderWidth;
+
+                let cornerRadius = chart.options.curvature;
+                if (cornerRadius > 20) {
+                  console.log('Whoa there buddy !');
+                  cornerRadius = 20;
+                }
+
+                if (!vm.horizontal) {
+                  left = vm.x - vm.width / 2;
+                  right = vm.x + vm.width / 2;
+                  top = vm.y;
+                  bottom = vm.base;
+
+                  signX = 1;
+                  signY = bottom > top ? 1 : -1;
+                  borderSkipped = vm.borderSkipped || 'bottom';
+                } else {
+                  left = vm.base;
+                  right = vm.x;
+                  top = vm.y - vm.height / 2;
+                  bottom = vm.y + vm.height / 2;
+
+                  signX = right > left ? 1 : -1;
+                  signY = 1;
+                  borderSkipped = vm.borderSkipped || 'left';
+                }
+
+                if (borderWidth) {
+                  const barSize = Math.min(Math.abs(left - right), Math.abs(top - bottom));
+
+                  borderWidth = borderWidth > barSize ? barSize : borderWidth;
+                  const halfStroke = borderWidth / 2;
+                  const borderLeft = left + (borderSkipped !== 'left' ? halfStroke + signX : 0);
+                  const borderRight = right + (borderSkipped !== 'right' ? halfStroke + signX : 0);
+                  const borderTop = top + (borderSkipped !== 'top' ? halfStroke + signY : 0);
+                  const borderBottom =
+                    bottom + (borderSkipped !== 'bottom' ? halfStroke + signY : 0);
+
+                  if (borderLeft !== borderRight) {
+                    top = borderTop;
+                    bottom = borderBottom;
+                  }
+
+                  if (borderTop !== borderBottom) {
+                    left = borderLeft;
+                    right = borderRight;
+                  }
+                }
+
+                ctx.beginPath();
+                ctx.fillStyle = vm.backgroundColor;
+                ctx.strokeStyle = vm.borderColor;
+                ctx.lineWidth = borderWidth;
+
+                const corners = [[left, bottom], [left, top], [right, top], [right, bottom]];
+
+                const border = ['bottom', 'left', 'top', 'right'];
+                let startCorner = border.indexOf(borderSkipped, 0);
+                if (startCorner === -1) {
+                  startCorner = 0;
+                }
+
+                function cornerAt(index) {
+                  return corners[(startCorner + index) % 4];
+                }
+
+                let corner = cornerAt(0);
+                let width, height, x, y, nextCornerId, nextCorner;
+                let xTL, xTR, yTL, yTR;
+                let xBL, xBR, yBL, yBR;
+                ctx.moveTo(corner[0], corner[1]);
+
+                for (let i = 1; i < 4; i++) {
+                  corner = cornerAt(i);
+                  nextCornerId = i + 1;
+                  if (nextCornerId === 4) {
+                    nextCornerId = 0;
+                  }
+                }
+              };
+            },
+          }}
         />
       </div>
     );
